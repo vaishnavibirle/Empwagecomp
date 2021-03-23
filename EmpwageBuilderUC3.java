41139a8f53268c2e7162c8876c63dd32d10315a2
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-03-23 23:01:34 +0530

    [vaishnavi] added EmpwageBuilderUC3.java

---
+++ EmpwageBuilderUC3.java
@@ -0,0 +1,20 @@
+public class EmpwageBuilderUC3  {
+	public static void main(String[] args)
+       {
+                //constants
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
+		int EMP_RATE_PER_HOUR = 20;
+		//variables
+		int empHrs = 0;
+		int empWage = 0;
+		//computation
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 6;
+		else if (empCheck == IS_PART_TIME)
+			empHrs = 8;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
+	}
+}
